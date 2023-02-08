911f4ebc90bd89afdfd2c88df32517cdef5824e2
Nacho <ignacio.moreno-frison@capgemini.com>
2023-02-08 10:55:13 +0100

    grunt build

--- js/Gruntfile.js
+++ js/Gruntfile.js
@@ -1,4 +1,8 @@
 module.exports=function(grunt){
+    require('time-grunt')(grunt);
+    require('jit-grunt')(grunt,{
+        useminPrepare: 'grunt-usemin'
+    });
     grunt.initConfig({
         sass:{
             dist:{
@@ -30,13 +34,123 @@ module.exports=function(grunt){
                 baseDir: './'
                 }
             }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: './',
+                    src: 'images/*.{png,gif,jpg,jpeg}',
+                    dest: 'dist/'
+                }]
+            }
+        },
+
+        copy: {
+            html: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: './',
+                    src: ['*.html'],
+                    dest: 'dist'
+                }]
+            }
+        },
+
+        clean: {
+            build: {
+                src: ['dist/']
+            }
+        },
+
+        cssmin: {
+            dist: {}
+        },
+
+        uglify:{
+            dist: {}
+        },
+
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length:20
+            },
+
+            release: {
+                // filerev:release hashes(md5) all assets (images,js and css)
+                // in dist directory
+                files: [{
+                    src:[
+                        'dist/js/*.js',
+                        'dist/css/*.css',
+                    ]
+                }]
+            }
+        },
+
+        concat: {
+            options:{
+                separator: ';'
+            },
+            dist: {}
+        },
+
+        useminPrepare: {
+            foo: {
+                dest: 'dist',
+                src: ['index.html','about.html','condiciones.html','contacto.html','precios.html']
+            },
+            options: {
+                flow: {
+                    steps: {
+                        css: ['cssmin'],
+                        js: ['uglify']
+                    },
+                    post: {
+                        css:[{
+                            name: 'cssmin',
+                            createConfig: function(context, block) {
+                                var generated = context.options.generated;
+                                generated.options= {
+                                    keepSpecialComments: 0,
+                                    rebase:false
+                                }
+                            }
+                        }]
+                    }
+                }
+            }
+        },
+
+        usemin: {
+            html: ['dist/index.html', 'dist/about.html','dist/precios.html','dist/contacto.html','dist/condiciones.html'],
+            options: {
+                assetsDir: ['dist', 'dist/css', 'dist/js']
+            }
         }
     },
     });
 
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    /* grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-imagemin'); */
     grunt.registerTask('css', ['sass']);
     grunt.registerTask('default', ['browserSync','watch']);
+    grunt.registerTask('img:compress',['imagemin']);
+    grunt.registerTask('build',[
+    'clean',
+    'copy',
+    'imagemin',
+    'useminPrepare',
+    'concat',
+    'cssmin',
+    'uglify',
+    'filerev',
+    'usemin'
+    ])
 };
